268d3d0f7fe2863a1376ede111f11c17295a15f8
Sangeetha Raju <sangeetha.raju@syncfusion.com>
2018-10-30 16:47:11 +0530

    Sample commit

---
+++ MaintainScrollPosition/MaintainScrollPosition/MaintainScrollPosition/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MaintainScrollPosition
+{
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Contacts> contactsInfo;
+        public ObservableCollection<Contacts> employeeInfo;
+
+        #region Properties
+
+        public ObservableCollection<Contacts> ContactsInfo
+        {
+            get
+            {
+                if (contactsInfo == null)
+                    GeneratePersonalContacts();
+                return contactsInfo;
+            }
+        }
+
+        public ObservableCollection<Contacts> EmployeeInfo
+        {
+            get
+            {
+                if (employeeInfo == null)
+                    GenerateEmployeeContacts();
+                return employeeInfo;
+            }
+        }
+
+        private void GenerateEmployeeContacts()
+        {
+            employeeInfo = new ObservableCollection<Contacts>();
+            Random r = new Random();
+            for (int i = 0; i < CustomerNames.Count(); i++)
+            {
+                var contact2 = new Contacts(CustomerNames[i], i.ToString());
+                contact2.ContactImage = ImageSource.FromResource("MaintainScrollPosition.Images.Image" + r.Next(0, 28) + ".png");
+                contact2.ContactType = "Employee";
+                EmployeeInfo.Add(contact2);
+            }
+        }
+
+        private void GeneratePersonalContacts()
+        {
+            contactsInfo = new ObservableCollection<Contacts>();
+            Random r = new Random();
+            for (int i = 0; i < CustomerNames.Count(); i++)
+            {
+                var contact1 = new Contacts(CustomerNames[i], i.ToString());
+                contact1.ContactImage = ImageSource.FromResource("MaintainScrollPosition.Images.Image" + r.Next(0, 28) + ".png");
+                contact1.ContactType = "Personal";
+                ContactsInfo.Add(contact1);
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public ContactsViewModel()
+        {
+        }
+
+        #endregion
+
+        #region Fields
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson"    ,
+            "Mason  "    ,
+            "Liam   "    ,
+            "Jacob  "    ,
+            "Jayden "    ,
+            "Ethan  "    ,
+            "Noah   "    ,
+            "Lucas  "    ,
+            "Logan  "    ,
+            "Caleb  "    ,
+            "Caden  "    ,
+            "Jack   "    ,
+            "Ryan   "    ,
+            "Connor "    ,
+            "Michael"    ,
+            "Elijah "    ,
+            "Brayden"    ,
+            "Benjamin"   ,
+            "Nicholas"   ,
+            "Alexander"  ,
+            "William"    ,
+            "Matthew"    ,
+            "James  "    ,
+            "Landon "    ,
+            "Nathan "    ,
+            "Dylan  "    ,
+            "Evan   "    ,
+            "Luke   "    ,
+            "Andrew "    ,
+            "Gabriel"    ,
+            "Gavin  "    ,
+            "Joshua "    ,
+            "Owen   "    ,
+            "Daniel "    ,
+            "Carter "    ,
+            "Tyler  "    ,
+            "Cameron"    ,
+            "Christian"  ,
+            "Wyatt  "    ,
+            "Henry  "    ,
+            "Eli    "    ,
+            "Joseph "    ,
+            "Max    "    ,
+            "Isaac  "    ,
+            "Samuel "    ,
+            "Anthony"    ,
+            "Grayson"    ,
+            "Zachary"    ,
+            "David  "    ,
+            "Christopher",
+            "John   "    ,
+            "Isaiah "    ,
+            "Levi   "    ,
+            "Jonathan"   ,
+            "Oliver "    ,
+            "Chase  "    ,
+            "Cooper "    ,
+            "Tristan"    ,
+            "Colton "    ,
+            "Austin "    ,
+            "Colin  "    ,
+            "Charlie"    ,
+            "Dominic"    ,
+            "Parker "    ,
+            "Hunter "    ,
+            "Thomas "    ,
+            "Alex   "    ,
+            "Ian    "    ,
+            "Jordan "    ,
+            "Cole   "    ,
+            "Julian "    ,
+            "Aaron  "    ,
+            "Carson "    ,
+            "Miles  "    ,
+            "Blake  "    ,
+            "Brody  "    ,
+            "Adam   "    ,
+            "Sebastian"  ,
+            "Adrian "    ,
+            "Nolan  "    ,
+            "Sean   "    ,
+            "Riley  "    ,
+            "Bentley"    ,
+            "Xavier "    ,
+            "Hayden "    ,
+            "Jeremiah"   ,
+            "Jason  "    ,
+            "Jake   "    ,
+            "Asher  "    ,
+            "Micah  "    ,
+            "Jace   "    ,
+            "Brandon"    ,
+            "Josiah "    ,
+            "Hudson "    ,
+            "Nathaniel"  ,
+            "Bryson "    ,
+            "Ryder  "    ,
+            "Justin "    ,
+            "Bryce  "    ,
+        };
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
